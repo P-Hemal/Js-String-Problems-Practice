321f6412a882103d1ae00a606de327b2552ad0e6
P-Hemal <abdulhakim.hemal.99@gmail.com>
2024-03-10 16:38:13 +0600

    abs

---
+++ math.js
@@ -0,0 +1,22 @@
+const result = Math.pow(3, 7);
+// console.log(result);
+
+const num1 = 25;
+const num2 = 45;
+const gap = Math.abs(num1 - num2);
+// console.log(gap)
+if(gap < 5){
+    console.log('its positive')
+}
+else{
+    console.log('its negative')
+}
+
+const number = 2.45875;
+const fullNumber = Math.ceil(number);
+// console.log(fullNumber)
+
+const number2 = 2.44657;
+const fullNumber2 = Math.floor(number2);
+// console.log(fullNumber2);
+
