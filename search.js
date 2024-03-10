c21b73009ecf07651899aba19a257ce49c496200
P-Hemal <abdulhakim.hemal.99@gmail.com>
2024-03-10 16:22:21 +0600

    split

--- search.js
+++ search.js
@@ -1,22 +1,25 @@
 const address = '597, Mohis Bathan Uttar para, rajshahi';
-const searchString = 'mohis';
-const addressLowerCase = address.toLowerCase();
-const doesExist = addressLowerCase.includes(searchString);
-console.log(doesExist);
+// const searchString = 'mohis';
+// const addressLowerCase = address.toLowerCase();
+// const doesExist = addressLowerCase.includes(searchString);
+// console.log(doesExist);
 
 
-// Index of
+// // Index of
 
-// console.log(address.indexOf('Bathan'));
-const insideString = 'para';
-if(address.indexOf(insideString) !== -1)(
-    console.log('inside the string')
-)
-else(
-    console.log('can not find')
-)
+// // console.log(address.indexOf('Bathan'));
+// const insideString = 'para';
+// if(address.indexOf(insideString) !== -1)(
+//     console.log('inside the string')
+// )
+// else(
+//     console.log('can not find')
+// )
 
 
-//  StartWith
+// //  StartWith
 
 console.log(address.startsWith(597));
+
+const searching = address.indexOf('Uttar');
+console.log(searching)
--- split-join.js
+++ split-join.js
@@ -1,7 +1,21 @@
 //  split
 
-const address = '597, Mohis Bathan Uttar para, rajshahi court : 6201, rajpara, rajshahi';
+const address = '597, Mohis Bathan Uttar para. rajshahi court : 6201. rajpara, rajshahi';
 const parts = address.split(' ');
 const sentences = address.split('.');
 const chars = address.split('');
-console.log(chars);
+// console.log(sentences);
+
+//  Slice
+const partial = address.slice(5, 17);
+// console.log(partial);
+
+
+//  Join
+const line = [
+    '597, Mohis Bathan Uttar para',
+    'rajshahi court : 6201',
+    'rajpara, rajshahi'
+  ]
+const newAddress = line.join(':');
+console.log(newAddress);
