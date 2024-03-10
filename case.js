a6cb2d888386ad991a95e11564a791e859111c73
P-Hemal <abdulhakim.hemal.99@gmail.com>
2024-03-10 15:03:03 +0600

    first commit

---
+++ case.js
@@ -0,0 +1,10 @@
+const userName = 'blackPink';
+const userInput = 'blackPinK';
+// console.log(userName);
+
+if(userName.toLowerCase === userInput.toLowerCase){
+    console.log('Valid User')
+}
+else{
+    console.log('Invalid User')
+}
---
+++ split-join.js
@@ -0,0 +1,7 @@
+//  split
+
+const address = '597, Mohis Bathan Uttar para, rajshahi court : 6201, rajpara, rajshahi';
+const parts = address.split(' ');
+const sentences = address.split('.');
+const chars = address.split('');
+console.log(chars);
