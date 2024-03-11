81723c88746ae780898d7ddabbb26ad69f485f01
P-Hemal <abdulhakim.hemal.99@gmail.com>
2024-03-11 12:16:51 +0600

    max3

---
+++ max3.js
@@ -0,0 +1,12 @@
+const jim = 84;
+const della = 97;
+const chinku = 97;
+if(jim > della && jim >chinku){
+    console.log('jim will get the cake')
+}
+else if(della > jim && della > chinku){
+    console.log('della will get the cake')
+}
+else{
+    console.log('chinku will get the cake')
+}
