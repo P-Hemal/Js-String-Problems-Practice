e93cc911f41cc6e0d7fbd53c1f264c013336ee75
P-Hemal <abdulhakim.hemal.99@gmail.com>
2024-03-12 12:34:50 +0600

    word reverse

--- split-join.js
+++ split-join.js
@@ -4,7 +4,7 @@ const address = '597, Mohis Bathan Uttar para. rajshahi court : 6201. rajpara, r
 const parts = address.split(' ');
 const sentences = address.split('.');
 const chars = address.split('');
-// console.log(sentences);
+console.log(parts);
 
 //  Slice
 const partial = address.slice(5, 17);
